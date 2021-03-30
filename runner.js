8c19ab79f986b26aaefce5a934d3f977f176a60e
Dylan Pinn <dylan.pinn@gmail.com>
2021-03-30 23:12:31 +1100

    feat: execute code inside test blocks (#8)
    Output success and failures in a simple way.
    
    Handle failing tests and allow tests to expect an exception.

--- runner.js
+++ runner.js
@@ -1,3 +1,34 @@
-function test() {}
+const GREEN = "\u001b[32m";
+const RED = "\u001b[31m";
+const RESET = "\u001b[39m";
 
-module.exports = test;
+let hasFailingTest = false;
+
+function test(description, callback) {
+  try {
+    process.stdout.write(` - ${description}`);
+    callback();
+    console.log(` ${GREEN}(ok)${RESET}`);
+  } catch (e) {
+    console.log(` ${RED}(fail)${RESET}`);
+    console.log(`   ${e.stack}`);
+    console.log(`   ${RED}${e.toString()}${RESET}`);
+
+    hasFailingTest = true;
+  }
+}
+
+function handleExit(code) {
+  if (code !== 0) {
+    return;
+  }
+  if (hasFailingTest) {
+    process.exit(1);
+  }
+}
+
+module.exports = (function () {
+  process.on("exit", handleExit);
+
+  return test;
+})();
