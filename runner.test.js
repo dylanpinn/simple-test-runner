6b3ff9e327b5267112875a13233588533872f682
Dylan Pinn <me@dylanpinn.com>
2021-03-30 21:29:53 +1100

    feat: execute code inside test blocks
    Output success and failures in a simple way.

--- runner.test.js
+++ runner.test.js
@@ -1,3 +1,12 @@
-var test = require("./runner");
+const assert = require("assert").strict;
+const test = require("./runner");
 
-test("handles test blocks", () => {});
+test("can expect values", () => {
+  assert.strictEqual(1 + 1, 2, "1 + 1 should equal 2");
+});
+
+test("can expect exceptions", () => {
+  assert.throws(() => {
+    throw new Error("This is an expected error");
+  }, /This is an expected error/);
+});
